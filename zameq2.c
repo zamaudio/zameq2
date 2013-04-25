d7afb71a3f0149094914109ab009833f95b97c76
Damien Zammit <damien@zamaudio.com>
2013-04-25 18:44:02 +1000

    Added highshelf filter and cleaned up code.

--- zameq2.c
+++ zameq2.c
@@ -21,7 +21,11 @@ typedef enum {
 	
 	ZAMEQ2_BOOSTDBL = 8,
 	ZAMEQ2_SLOPEDBL = 9,
-	ZAMEQ2_FREQL = 10
+	ZAMEQ2_FREQL = 10,
+
+	ZAMEQ2_BOOSTDBH = 11,
+	ZAMEQ2_SLOPEDBH = 12,
+	ZAMEQ2_FREQH = 13
 } PortIndex;
 
 typedef struct {
@@ -40,16 +44,20 @@ typedef struct {
 	float* slopedbl;
 	float* freql;
 
+	float* boostdbh;
+	float* slopedbh;
+	float* freqh;
+
 	float x1,x2,y1,y2;
 	float x1a,x2a,y1a,y2a;
-	float zln1a,zln2a,zld1a,zld2a;
-	float zln1b,zln2b,zld1b,zld2b;
+	float zln1,zln2,zld1,zld2;
+	float zhn1,zhn2,zhd1,zhd2;
 	float a0x,a1x,a2x,b0x,b1x,b2x,gainx;
 	float a0y,a1y,a2y,b0y,b1y,b2y,gainy;
-	float B[3][5];
-	float A[3][5];
-	float Bh[3][5];
-	float Ah[3][5];
+	float Bl[3];
+	float Al[3];
+	float Bh[3];
+	float Ah[3];
 	float srate;
 } ZamEQ2;
 
@@ -68,10 +76,13 @@ instantiate(const LV2_Descriptor*     descriptor,
 	zameq2->x1a=zameq2->x2a=zameq2->y1a=zameq2->y2a=0.f;
 	zameq2->a0x=zameq2->a1x=zameq2->a2x=zameq2->b0x=zameq2->b1x=zameq2->b2x=zameq2->gainx=0.f;
 	zameq2->a0y=zameq2->a1y=zameq2->a2y=zameq2->b0y=zameq2->b1y=zameq2->b2y=zameq2->gainy=0.f;
-	for (i = 0; i < 5; ++i) {
-		zameq2->B[0][i] = zameq2->A[0][i] = zameq2->Bh[0][i] = zameq2->Ah[0][i] = 0.f;
-		zameq2->B[1][i] = zameq2->A[1][i] = zameq2->Bh[1][i] = zameq2->Ah[1][i] = 0.f;
-		zameq2->B[2][i] = zameq2->A[2][i] = zameq2->Bh[2][i] = zameq2->Ah[2][i] = 0.f;
+	zameq2->zln1=zameq2->zln2=zameq2->zld1=zameq2->zld2=0.f;
+	zameq2->zhn1=zameq2->zhn2=zameq2->zhd1=zameq2->zhd2=0.f;
+
+	for (i = 0; i < 3; ++i) {
+		zameq2->Bl[i] = zameq2->Al[i] = zameq2->Bh[i] = zameq2->Ah[i] = 0.f;
+		zameq2->Bl[i] = zameq2->Al[i] = zameq2->Bh[i] = zameq2->Ah[i] = 0.f;
+		zameq2->Bl[i] = zameq2->Al[i] = zameq2->Bh[i] = zameq2->Ah[i] = 0.f;
 	}
 
 	return (LV2_Handle)zameq2;
@@ -117,6 +128,15 @@ connect_port(LV2_Handle instance,
 		break;
 	case ZAMEQ2_FREQL:
 		zameq2->freql = (float*)data;
+		break;
+	case ZAMEQ2_BOOSTDBH:
+		zameq2->boostdbh = (float*)data;
+		break;
+	case ZAMEQ2_SLOPEDBH:
+		zameq2->slopedbh = (float*)data;
+		break;
+	case ZAMEQ2_FREQH:
+		zameq2->freqh = (float*)data;
 		break;
 	}
 }
@@ -199,152 +219,10 @@ peq(float G0, float G, float GB, float w0, float Dw,
 }
 
 static bool
-shelfeq(float G0, float G, float GB, float w0, float Dw, float q,
-		float B[][5], float A[][5], float Bh[][5], float Ah[][5]) {
-	float r,L,c0,WB,e,g,g0,b,D,phi,si,b0h,b1h,b2h,a1h,a2h,b0,b1,b2,b3,b4,a0,a1,a2,a3,a4,alpha;
-	int N = 2;
-	int i;
-
-	G0 = powf(10.f,G0/20.f);
+lowshelfeq(float G0, float G, float GB, float w0, float Dw, float q,
+		float B[], float A[]) {
+ 	float alpha,b0,b1,b2,a0,a1,a2;
 	G = powf(10.f,G/20.f); 
-	GB = powf(10.f,GB/20.f); 
-
-	r = N % 2; L = (N-r)/2.f;
-
-	Bh[0][0] = 1.f;
-	Bh[0][1] = 0.f;
-	Bh[0][2] = 0.f;
-
-	Ah[0][0] = 1.f;
-	Ah[0][1] = 0.f;
-	Ah[0][2] = 0.f;
-		
-	B[0][0] = 1.f;
-	B[0][1] = 0.f;
-	B[0][2] = 0.f;
-	B[0][3] = 0.f;
-	B[0][4] = 0.f;
-	
-	A[0][0] = 1.f;
-	A[0][1] = 0.f;
-	A[0][2] = 0.f;
-	A[0][3] = 0.f;
-	A[0][4] = 0.f;
-
-	//if (G==G0) 
-	//	return false; 
-
-	c0 = cos(w0); 
-
-	if (w0==0.f) 
-		c0 = 1.f;
-
-	if (w0==M_PI/2.f)
-		c0 = 0.f;
-
-	if (w0==M_PI)
-		c0 = -1.f;
-
-	//WB = tan(Dw/2.f);
-	WB = tan(Dw/(q*q));
-	e = sqrt((G*G - GB*GB)/(GB*GB - G0*G0)); 
-	g = powf(G,1.f/N); 
-	g0 = powf(G0,1.f/N); 
-
-	//Butterworth
-	b = WB / (powf(e,1.f/N));
-  
-	if (r==1) {                                       
-		D = b + 1.f;
-		Bh[0][0] = (g*b+g0)/D;
-		Bh[0][1] = (g*b-g0)/D;
-		Bh[0][2] = 0.f;
-
-    		Ah[0][0] = 1.f;
-    		Ah[0][1] = (b-1.f)/D;
-    		Ah[0][2] = 0.f;
-
-    		B[0][0] = (g0+g*b)/D;
-    		B[0][1] = (-2.f*g0*c0)/D;
-    		B[0][2] = (g0-g*b)/D;
-    		B[0][3] = 0.f; 
-    		B[0][4] = 0.f;
-
-    		A[0][0] = 1.f;
-    		A[0][1] = (-2.f*c0)/D;
-    		A[0][2] = (1.f-b)/D;
-    		A[0][3] = 0.f; 
-    		A[0][4] = 0.f;
-	}    
-
-	for (i = 0; i <= L; ++i) {
-		phi = (2.f*i-1.f)*M_PI/(2.f*N);                                
-		si = sin(phi);
-		D = b*b + 2.f*b*si + 1.f;
-		b0h = (g*g*b*b + 2.f*g0*g*b*si + g0*g0)/D;        
-		b1h = 2.f*(g*g*b*b - g0*g0)/D;
-		b2h = (g*g*b*b - 2.f*g0*g*b*si + g0*g0)/D;
-		a1h = 2.f*(b*b - 1.f)/D;
-		a2h = (b*b - 2.f*b*si + 1.f)/D;
-		
-	        Bh[1+i][0] = b0h;
-	        Bh[1+i][1] = b1h;
-	        Bh[1+i][2] = b2h;
-
-	        Ah[1+i][0] = 1.f;
-		Ah[1+i][1] = a1h;
-		Ah[1+i][2] = a2h;
-
-		b0 = (g*g*b*b + g0*g0 + 2.f*g*g0*si*b)/D;
-		b1 = -4.f*c0*(g0*g0 + g*g0*si*b)/D;
-		b2 = 2.f*((1.f+2.f*c0*c0)*g0*g0 - g*g*b*b)/D;
-		b3 = -4.f*c0*(g0*g0 - g*g0*si*b)/D;
-		b4 = (g*g*b*b + g0*g0 - 2.f*g*g0*si*b)/D;
-		a1 = -4.f*c0*(1.f + si*b)/D;
-		a2 = 2.f*(1.f+2.f*c0*c0 - b*b)/D;
-		a3 = -4.f*c0*(1.f - si*b)/D;
-		a4 = (b*b - 2.f*si*b + 1.f)/D;
-
-		B[1+i][0] = b0;
-		B[1+i][1] = b1;
-		B[1+i][2] = b2;
-		B[1+i][3] = b3;
-		B[1+i][4] = b4;
-		A[1+i][0] = 1.f;
-		A[1+i][1] = a1;
-		A[1+i][2] = a2;
-		A[1+i][3] = a3;
-		A[1+i][4] = a4;
-	}
-
-	// LP or HP shelving filter
-	if (c0==1.f || c0==-1.f)  { 
-		for (i = 1; i <= L+1; ++i) {
-			B[i][0] = Bh[i][0];
-        	        B[i][1] = Bh[i][1]*c0;
-	                B[i][2] = Bh[i][2];
-	                B[i][3] = 0.f;
-	                B[i][4] = 0.f;
-	                A[i][0] = Ah[i][0];
-	                A[i][1] = Ah[i][1]*c0;
-	                A[i][2] = Ah[i][2];
-	                A[i][3] = 0.f;
-	                A[i][4] = 0.f;
-
-			Bh[i][3] = 0.f;
-			Bh[i][4] = 0.f;
-			Ah[i][3] = 0.f;
-			Ah[i][4] = 0.f;
-			sanitize_denormal(B[i][1]);
-			sanitize_denormal(B[i][2]);
-			sanitize_denormal(A[i][1]);
-			sanitize_denormal(A[i][2]);
-			if (is_nan(B[i][0])) { B[i][0] = 1.f; }
-
-		}
-	}
-
- 
 	float AA  = sqrt(G);
 	
 	alpha = sin(w0)/2.f * sqrt( (AA + 1.f/AA)*(1.f/q - 1.f) + 2.f );
@@ -355,19 +233,39 @@ shelfeq(float G0, float G, float GB, float w0, float Dw, float q,
         a1 =   -2.f*( (AA-1.f) + (AA+1.f)*cos(w0)                   );
         a2 =        (AA+1.f) + (AA-1.f)*cos(w0) - 2.f*sqrt(AA)*alpha;
 	
-	B[2][0] = b0/a0;
-        B[2][1] = b1/a0;
-        B[2][2] = b2/a0;
-        A[2][0] = 1.f;
-        A[2][1] = a1/a0;
-        A[2][2] = a2/a0;
+	B[0] = b0/a0;
+        B[1] = b1/a0;
+        B[2] = b2/a0;
+        A[0] = 1.f;
+        A[1] = a1/a0;
+        A[2] = a2/a0;
 
 	return true;
 }
 
-static float
-arcsinh (float x) {
-	return (log(2.f*x+sqrt(x*x+1.f)));
+static bool
+highshelfeq(float G0, float G, float GB, float w0, float Dw, float q,
+		float B[], float A[]) {
+        float alpha,b0,b1,b2,a0,a1,a2;
+        G = powf(10.f,G/20.f);
+        float AA  = sqrt(G);
+
+        alpha = sin(w0)/2.f * sqrt( (AA + 1.f/AA)*(1.f/q - 1.f) + 2.f );
+        b0 =    AA*( (AA+1.f) + (AA-1.f)*cos(w0) + 2.f*sqrt(AA)*alpha );
+        b1 =  -2.f*AA*( (AA-1.f) + (AA+1.f)*cos(w0)                   );
+        b2 =    AA*( (AA+1.f) + (AA-1.f)*cos(w0) - 2.f*sqrt(AA)*alpha );
+        a0 =        (AA+1.f) - (AA-1.f)*cos(w0) + 2.f*sqrt(AA)*alpha;
+        a1 =   2.f*( (AA-1.f) - (AA+1.f)*cos(w0)                   );
+        a2 =        (AA+1.f) - (AA-1.f)*cos(w0) - 2.f*sqrt(AA)*alpha;
+
+        B[0] = b0/a0;
+        B[1] = b1/a0;
+        B[2] = b2/a0;
+        A[0] = 1.f;
+        A[1] = a1/a0;
+        A[2] = a2/a0;
+
+        return true;
 }
 
 static void
@@ -390,6 +288,10 @@ run(LV2_Handle instance, uint32_t n_samples)
 	const float        slopedbl = *(zameq2->slopedbl);
 	const float        freql = *(zameq2->freql);
 
+	const float        boostdbh = *(zameq2->boostdbh);
+	const float        slopedbh = *(zameq2->slopedbh);
+	const float        freqh = *(zameq2->freqh);
+
 	float dcgain = 1.f;
 	
 	float boost1 = from_dB(boostdb1);
@@ -405,31 +307,22 @@ run(LV2_Handle instance, uint32_t n_samples)
 	float bw2 = fc2 / q2;
 
 	float boostl = from_dB(boostdbl);
-	float fcl = zameq2->srate/4.f;
-	float w0l = 0.f;
-	float Al = sqrt(boostl);
-	//float ql = 1.f / (sqrt((Al+1.f/Al)*(1.f/slopel-1.f)+2.f)); 
-	
+	float All = sqrt(boostl);
 	float bwl = 2.f*M_PI*freql/ zameq2->srate;
-
-	//bwl = 2.f/log(2.f)*arcsinh(1.f/(2.f*ql));
+	float bwgaindbl = to_dB(All);
 	
+	float boosth = from_dB(boostdbh);
+	float Ahh = sqrt(boosth);
+	float bwh = 2.f*M_PI*freqh/ zameq2->srate;
+	float bwgaindbh = to_dB(Ahh);
 
-	float bwgaindbl = to_dB(Al);
-	//float bwgaindbl = to_dB((boostl-dcgain)*exp(-1.f/ql)+dcgain);
-	
 	peq(dcgain,boost1,bwgain1,w01,bw1,&zameq2->a0x,&zameq2->a1x,&zameq2->a2x,&zameq2->b0x,&zameq2->b1x,&zameq2->b2x,&zameq2->gainx);
 	peq(dcgain,boost2,bwgain2,w02,bw2,&zameq2->a0y,&zameq2->a1y,&zameq2->a2y,&zameq2->b0y,&zameq2->b1y,&zameq2->b2y,&zameq2->gainy);
-	shelfeq(0.f,boostdbl,bwgaindbl,2.f*M_PI*freql/zameq2->srate,bwl,slopedbl,zameq2->B,zameq2->A,zameq2->Bh,zameq2->Ah);
-
-	//printf("Gdb=%f fc=%f A=%f Q=%f bw=%f GBdb=%f\n",boostdbl,fcl, Al, ql, bwl, bwgaindbl);
-	printf("B=[%f, %f, %f; %f, %f, %f]\n",zameq2->B[1][0],zameq2->B[1][1],zameq2->B[1][2],zameq2->B[2][0],zameq2->B[2][1],zameq2->B[2][2]);
-	printf("A=[%f, %f, %f; %f, %f, %f]\n",zameq2->A[1][0],zameq2->A[1][1],zameq2->A[1][2],zameq2->A[2][0],zameq2->A[2][1],zameq2->A[2][2]);
-
-		
+	lowshelfeq(0.f,boostdbl,bwgaindbl,2.f*M_PI*freql/zameq2->srate,bwl,slopedbl,zameq2->Bl,zameq2->Al);
+	highshelfeq(0.f,boostdbh,bwgaindbh,2.f*M_PI*freqh/zameq2->srate,bwh,slopedbh,zameq2->Bh,zameq2->Ah);
 
 	for (uint32_t pos = 0; pos < n_samples; pos++) {
-		float tmp,tmpla, tmplb;
+		float tmp,tmpl, tmph;
 		float in = input[pos];
 		sanitize_denormal(zameq2->x1);
 		sanitize_denormal(zameq2->x2);
@@ -439,43 +332,43 @@ run(LV2_Handle instance, uint32_t n_samples)
 		sanitize_denormal(zameq2->x2a);
 		sanitize_denormal(zameq2->y1a);
 		sanitize_denormal(zameq2->y2a);
-		sanitize_denormal(zameq2->zln1a);
-		sanitize_denormal(zameq2->zln2a);
-		sanitize_denormal(zameq2->zld1a);
-		sanitize_denormal(zameq2->zld2a);
-		sanitize_denormal(zameq2->zln1b);
-		sanitize_denormal(zameq2->zln2b);
-		sanitize_denormal(zameq2->zld1b);
-		sanitize_denormal(zameq2->zld2b);
+		sanitize_denormal(zameq2->zln1);
+		sanitize_denormal(zameq2->zln2);
+		sanitize_denormal(zameq2->zld1);
+		sanitize_denormal(zameq2->zld2);
+		sanitize_denormal(zameq2->zhn1);
+		sanitize_denormal(zameq2->zhn2);
+		sanitize_denormal(zameq2->zhd1);
+		sanitize_denormal(zameq2->zhd2);
 		sanitize_denormal(in);
 
-		//lowshelf a
-		tmpla = in * zameq2->B[2][0] + 
-			zameq2->zln1a * zameq2->B[2][1] +
-			zameq2->zln2a * zameq2->B[2][2] -
-			zameq2->zld1a * zameq2->A[2][1] -
-			zameq2->zld2a * zameq2->A[2][2];
-		zameq2->zln2a = zameq2->zln1a;
-		zameq2->zld2a = zameq2->zld1a;
-		zameq2->zln1a = in;
-		zameq2->zld1a = tmpla;
+		//lowshelf
+		tmpl = in * zameq2->Bl[0] + 
+			zameq2->zln1 * zameq2->Bl[1] +
+			zameq2->zln2 * zameq2->Bl[2] -
+			zameq2->zld1 * zameq2->Al[1] -
+			zameq2->zld2 * zameq2->Al[2];
+		zameq2->zln2 = zameq2->zln1;
+		zameq2->zld2 = zameq2->zld1;
+		zameq2->zln1 = in;
+		zameq2->zld1 = tmpl;
 		
-		//lowshelf b
-/*		tmplb = tmpla * zameq2->B[2][0] + 
-			zameq2->zln1b * zameq2->B[2][1] +
-			zameq2->zln2b * zameq2->B[2][2] -
-			zameq2->zld1b * zameq2->A[2][1] -
-			zameq2->zld2b * zameq2->A[2][2];
-		zameq2->zln2b = zameq2->zln1b;
-		zameq2->zld2b = zameq2->zld1b;
-		zameq2->zln1b = tmpla;
-		zameq2->zld1b = tmplb;
-*/		
+		//highshelf
+		tmph = tmpl * zameq2->Bh[0] + 
+			zameq2->zhn1 * zameq2->Bh[1] +
+			zameq2->zhn2 * zameq2->Bh[2] -
+			zameq2->zhd1 * zameq2->Ah[1] -
+			zameq2->zhd2 * zameq2->Ah[2];
+		zameq2->zhn2 = zameq2->zhn1;
+		zameq2->zhd2 = zameq2->zhd1;
+		zameq2->zhn1 = tmpl;
+		zameq2->zhd1 = tmph;
+		
 		//parametric1
-		tmp = tmpla * zameq2->b0x + zameq2->x1 * zameq2->b1x + zameq2->x2 * zameq2->b2x - zameq2->y1 * zameq2->a1x - zameq2->y2 * zameq2->a2x;
+		tmp = tmph * zameq2->b0x + zameq2->x1 * zameq2->b1x + zameq2->x2 * zameq2->b2x - zameq2->y1 * zameq2->a1x - zameq2->y2 * zameq2->a2x;
 		zameq2->x2 = zameq2->x1;
 		zameq2->y2 = zameq2->y1;
-		zameq2->x1 = tmpla;
+		zameq2->x1 = tmph;
 		zameq2->y1 = tmp;
 		
 		//parametric2
